2e9e670ee714e32a0dea387762559222c4f2fe94
hasnakhann26-sketch <hasnakhann26@gmail.com>
2026-08-14 22:16:46 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-        base: '/pathlight/',
-plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
